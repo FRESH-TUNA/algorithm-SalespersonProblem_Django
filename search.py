679a4a1c16ad621ca6c66850b0b8dc1c1b7c8448
김동원 <lunacircle4@likelion.org>
2019-05-09 12:57:31 +0000

    finish no.1

--- search.py
+++ search.py
@@ -3,49 +3,21 @@ class roads:
     def __init__(self):
          self.city = [
              '서울', '인천', '대구', '인천',
-             '대전', '광주', '울산', '세종'
+             '대전'
          ]
          self.weight = [
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1]
+            [0, 14, 4, 10, 20],
+            [14, 0, 7, 8, 7],
+            [4, 5, 0, 7, 16],
+            [11, 7, 9, 0, 2],
+            [18, 7, 17, 4, 0]
          ]
-         self.tour = [] * len(self.city)
+         self.tour = [0] * len(self.city)
          self.bestLength = sys.maxsize
          self.bestTour = None
-    
-    def travel(self, index):
-        if index == len(self.city) - 1:
-            currentPos = self.tour[index]
-            resultLength = calcLength(len(self.tour)) + self.weight[currentPos][0]
-            if resultLength < self.bestLength:
-                self.bestLength = resultLength
-                self.bestTour = copy.deepcopy(self.tour)
-                return
-        if not promising():
-            return
-        for cityIndex in range(len(city)):
-            if not visited(index, cityIndex):
-                self.tour[index + 1] = cityIndex
-                travel(index + 1)
-    
-    def calcLength(self, last):
-        totalLength = 0
-        prev = self.tour[0]
-        i = 1
-        while i < last:
-            totalLength = totalLength + self.weight[prev][self.tour[i]]
-            prev = self.tour[i]
-            i = i + 1
-        return totalLength
-
+            
     def promising(self, index):
-        if calcLength(index) > self.bestLength:
+        if self.calcLength(index) < self.bestLength:
             return True
         else:
             return False
@@ -57,6 +29,35 @@ class roads:
                 return True
             i = i + 1
         return False
+    
+    def calcLength(self, last):
+        totalLength = 0
+        prev = self.tour[0]
+        i = 1
+        while i < last:
+            totalLength = totalLength + self.weight[prev][self.tour[i]]
+            prev = self.tour[i]
+            i = i + 1
+        return totalLength
+    
+    def travel(self, index):
+        if index == len(self.city) - 1:
+            currentPos = self.tour[index]
+            resultLength = self.calcLength(len(self.tour)) + self.weight[currentPos][0]
+            if resultLength < self.bestLength:
+                self.bestLength = resultLength
+                self.bestTour = copy.deepcopy(self.tour)
+                return
+        if not self.promising(index):
+            return
+        for cityIndex in range(len(self.city)):
+            if not self.visited(index, cityIndex):
+                self.tour[index + 1] = cityIndex
+                self.travel(index + 1)
+    
+
+
+  
 
             
         
---
+++ service.py
@@ -0,0 +1,5 @@
+import search
+
+newroads = search.roads()
+newroads.travel(0)
+print(newroads.bestLength)
